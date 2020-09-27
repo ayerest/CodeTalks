b48545d8c305f4e683b5fe370d369460f7f5d663
ayerest <irisiseve@gmail.com>
2020-09-27 16:58:54 -0700

    removing unused material ui elements

--- react-hooks-talk/src/Game/FunctionalComponent.jsx
+++ react-hooks-talk/src/Game/FunctionalComponent.jsx
@@ -3,8 +3,6 @@ import * as actions from "../GameStore/Actions/FunctionalActions";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { Grid } from "@material-ui/core";
-import List from "@material-ui/core/List";
-import ListItemText from "@material-ui/core/ListItemText";
 
 const FunctionalComponent = () => {
   return (
